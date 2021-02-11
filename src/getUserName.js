19c2bea57e255e1429542b322ccd5c079e15889e
georgesimos <admin@georgesimos.com>
2021-02-11 17:45:24 +0200

    use commonJS named exports

---
+++ src/getUserName.js
@@ -0,0 +1,7 @@
+/**
+ *
+ * @param {string} name
+ */
+const getUserName = (name) => `User Name is : ${name}`;
+
+module.exports = getUserName;
---
+++ src/userAddress.js
@@ -0,0 +1,5 @@
+const address = "address";
+const city = "city";
+
+exports.address = address;
+exports.city = city;
